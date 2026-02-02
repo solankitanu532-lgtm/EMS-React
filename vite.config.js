6591c3985102bb4bd1f568ca5c98f8db78ffd55c
solankitanu532-lgtm <solankitanu532@gamil.com>
2026-02-02 21:35:20 +0530

    Initial Commit

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react() ,tailwindcss(),],
-  base: '/EMS-React/'  // <-- Ye repo name hona chahiyen
+  base: '/'  // <-- Ye repo name hona chahiyen
 })
